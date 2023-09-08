10494e3999d214719c8ebbe172e0ff0814bf29bf
jty-AMSS <81076228+jty-AMSS@users.noreply.github.com>
2023-09-08 14:54:45 +0800

    Update ComputeSOSBySDPNAL.m

--- ComputeSOSBySDPNAL.m
+++ ComputeSOSBySDPNAL.m
@@ -1,11 +1,13 @@
-function [Q,lb]=ComputeSOSBySDPNAL(A,b)
+function [Q,lb]=ComputeSOSBySDPNAL(A,b,ADMmaxiter)
 m=size(A,2);
 m=sqrt(m);
 opti=SDPNALplus_parameters;
 opti.maxtime=1e6;
 % opti.printlevel=2;
 % opti.maxtime=2*m;
+if nargin==2
 ADMmaxiter=max(round(m),300);%3*m
+end
 opti.ADMmaxiter=ADMmaxiter;
 opti.maxiter=1;
 [blk,At,C,b]=CVX2SDPNALPlus(A,b);
@@ -23,26 +25,6 @@ mineig_Q=min(mineig_Q);
 Q=Q-mineig_Q*eye(Qn);
 err=A*Q(:)-b;
 err=norm(err,1);
-% disp('err')
-% disp(err)
-% for i=1:6
-%     if(err<0.5)
-%         break
-%     end
-%     disp(i)
-%     [obj,X,s,y,S,Z,ybar,v,info,runhist]= sdpnalplus(blk,At,C,b,[],[],[],[],[],opti,X,s,y,S,Z,ybar,v);
-%     Q=X{1};
-%     Qn=length(Q);
-%     mineig_Q=eig(Q);
-%     mineig_Q=min(mineig_Q);
-%     Q=Q-mineig_Q*eye(Qn);
-%     err=A*Q(:)-b;
-%     err=norm(err,1);
-%     disp('err')
-%     disp(err)
-%
-% end
-% [obj,X,s,y,S,Z,ybar,v,info,runhist]= sdpnalplus(blk,At,C,b,[],[],[],[],[],opti,X,s,y,S,Z,ybar,v);
 lb=norm(A*Q(:)-b,1)+trace(Q)- length(Q)*lambda_min(Q);
 
 end
